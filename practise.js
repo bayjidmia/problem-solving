7ea8fe91c15912a02e3d63debffbae945100a5f6
bayjidmia <mdbayjidmia982@gmail.com>
2026-04-08 09:47:15 +0600

    added pb3

--- practise.js
+++ practise.js
@@ -1,24 +1,38 @@
 //Problem : 1 - find largest Number from array
 let arr = [1, 2, 3, 4, 5];
-console.log(arr[1]);
+
 let largest = arr[0];
 for (let i = 1; i < arr.length; i++) {
   // memorize : increasing only index value in the loop
-  console.log(arr[i]);
+  // console.log(arr[i]);
   if (arr[i] > largest) {
     largest = arr[i];
   }
 }
-console.log(largest);
+// console.log(largest);
 
 // Problem : 2 - find smallest Number from array
 
 let arr2 = [12, 14, 15, 19, 200, 300, 1];
 let smallest = arr2[0];
 for (let i = 1; i < arr2.length; i++) {
-  console.log(arr2[i]);
   if (arr2[i] < smallest) {
     smallest = arr2[i];
   }
 }
-console.log(smallest);
+// console.log(smallest);
+
+// Problem : 3 - find second largest Number from array
+let arr3 = [95, 11, 18, 25, 96, 24, 100];
+let largest1 = -Infinity;
+let secondlargest = -Infinity;
+for (let i = 0; i < arr3.length; i++) {
+  if (arr3[i] > largest1) {
+    secondlargest = largest1;
+    largest1 = arr3[i];
+  }
+  if (arr3[i] > secondlargest && arr3[i] != largest1) {
+    secondlargest = arr3[i];
+  }
+}
+console.log(secondlargest);
